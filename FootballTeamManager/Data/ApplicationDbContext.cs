aba0a6a8e533449328d920ae3cc29f76e91e0e8e
ArteMaxL <artemiolucero@gmail.com>
2023-12-06 22:13:38 -0300

    Add project files.

---
+++ FootballTeamManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FootballTeamManager.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballTeamManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Equipo> Equipo { get; set; }   
+    }
+}
---
+++ FootballTeamManager/Modelos/Equipo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootballTeamManager.Modelos
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
