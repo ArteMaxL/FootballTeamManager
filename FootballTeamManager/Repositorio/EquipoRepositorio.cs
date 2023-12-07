8864e2c4ceb52a98d9bddfd649536afa9c4966bf
ArteMaxL <artemiolucero@gmail.com>
2023-12-06 23:07:47 -0300

    Repositorio Equipo

---
+++ FootballTeamManager/Repositorio/EquipoRepositorio.cs
@@ -0,0 +1,64 @@
+﻿using FootballTeamManager.Data;
+using FootballTeamManager.Modelos;
+using FootballTeamManager.Repositorio.IRepositorio;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FootballTeamManager.Repositorio
+{
+    public class EquipoRepositorio : IEquipoRepositorio
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EquipoRepositorio(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool ActualizarEquipo(Equipo modelo)
+        {
+            modelo.FechaCreacion = DateTime.Now;
+            _context.Equipo.Update(modelo);
+            return Guardar();
+        }
+
+        public bool BorrarEquipo(Equipo modelo)
+        {
+            _context.Equipo.Remove(modelo);
+            return Guardar();
+        }
+
+        public bool CrearEquipo(Equipo modelo)
+        {
+            modelo.FechaCreacion = DateTime.Now;
+            _context.Equipo.Add(modelo);
+            return Guardar();
+        }
+
+        public bool ExisteEquipo(string nombre)
+        {
+            bool valor = _context.Equipo.Any(x => x.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            return valor;
+        }
+
+        public bool ExisteEquipo(int id)
+        {
+            bool valor = _context.Equipo.Any(x => x.Id == id);
+            return valor;
+        }
+
+        public ICollection<Equipo> GetEquipo()
+        {
+            return _context.Equipo.OrderBy(x => x.Nombre).ToList();
+        }
+
+        public Equipo GetEquipo(int equipoId)
+        {
+            return _context.Equipo.FirstOrDefault(x => x.Id == equipoId);
+        }
+
+        public bool Guardar()
+        {
+            return _context.SaveChanges() >= 0 ? true : false;
+        }
+    }
+}
---
+++ FootballTeamManager/Repositorio/IRepositorio/IEquipoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using FootballTeamManager.Modelos;
+
+namespace FootballTeamManager.Repositorio.IRepositorio
+{
+    public interface IEquipoRepositorio
+    {
+        ICollection<Equipo> GetEquipo();
+        Equipo GetEquipo(int equipoId);
+        bool ExisteEquipo(string nombre);
+        bool ExisteEquipo(int id);
+        bool CrearEquipo(Equipo modelo);
+        bool ActualizarEquipo(Equipo modelo);
+        bool BorrarEquipo(Equipo modelo);
+        bool Guardar();
+    }
+}
