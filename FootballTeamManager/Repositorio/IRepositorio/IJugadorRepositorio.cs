963ff4ef3d1919115e9f3bfe10172c34735ecd1f
ArteMaxL <artemiolucero@gmail.com>
2023-12-07 22:53:41 -0300

    Repositorio para Jugador

---
+++ FootballTeamManager/Repositorio/IRepositorio/IJugadorRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using FootballTeamManager.Modelos;
+
+namespace FootballTeamManager.Repositorio.IRepositorio
+{
+    public interface IJugadorRepositorio
+    {
+        ICollection<Jugador> GetJugadores();
+        Jugador GetJugador(int jugadorId);
+        bool ExisteJugador(string nombre);
+        bool ExisteJugador(int id);
+        bool CrearJugador(Jugador modelo);
+        bool ActualizarJugador(Jugador modelo);
+        bool BorrarJugador(Jugador modelo);
+        ICollection<Jugador> GetJugadoresPorEquipo(int equipoId);
+        ICollection<Jugador> BuscarJugador(string nombre);
+        bool Guardar();
+    }
+}
---
+++ FootballTeamManager/Repositorio/JugadorRepositorio.cs
@@ -0,0 +1,80 @@
+﻿using FootballTeamManager.Data;
+using FootballTeamManager.Modelos;
+using FootballTeamManager.Repositorio.IRepositorio;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballTeamManager.Repositorio
+{
+    public class JugadorRepositorio : IJugadorRepositorio
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JugadorRepositorio(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool ActualizarJugador(Jugador modelo)
+        {
+            modelo.FechaCreacion = DateTime.Now;
+            _context.Jugador.Update(modelo);
+            return Guardar();
+        }
+
+        public bool BorrarJugador(Jugador modelo)
+        {
+            _context.Jugador.Remove(modelo);
+            return Guardar();
+        }
+
+        public bool CrearJugador(Jugador modelo)
+        {
+            modelo.FechaCreacion = DateTime.Now;
+            modelo.EstaActivo = true;
+            _context.Jugador.Add(modelo);
+            return Guardar();
+        }
+
+        public bool ExisteJugador(string nombre)
+        {
+            bool valor = _context.Jugador.Any(x => x.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            return valor;
+        }
+
+        public bool ExisteJugador(int id)
+        {
+            bool valor = _context.Jugador.Any(x => x.Id == id);
+            return valor;
+        }
+
+        public ICollection<Jugador> GetJugadores()
+        {
+            return _context.Jugador.OrderBy(x => x.Nombre).ToList();
+        }
+
+        public Jugador GetJugador(int equipoId)
+        {
+            return _context.Jugador.FirstOrDefault(x => x.Id == equipoId);
+        }
+
+        public ICollection<Jugador> GetJugadoresPorEquipo(int equipoId)
+        {
+            return _context.Jugador.Include(e => e.Equipo).Where(e => e.EquipoId == equipoId).ToList();
+        }
+
+        public ICollection<Jugador> BuscarJugador(string nombre)
+        {
+            IQueryable<Jugador> query = _context.Jugador;
+
+            if (!string.IsNullOrEmpty(nombre)) query = query.Where(j => j.Nombre.Contains(nombre));
+
+            return query.ToList();
+        }
+
+        public bool Guardar()
+        {
+            return _context.SaveChanges() >= 0 ? true : false;
+        }
+
+    }
+}
