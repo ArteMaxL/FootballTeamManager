8efdc3370e0fec5822e43fc0baeb529803865547
ArteMaxL <artemiolucero@gmail.com>
2023-12-07 21:52:21 -0300

    Finalizado Crud Equipo

--- FootballTeamManager/Controllers/EquipoController.cs
+++ FootballTeamManager/Controllers/EquipoController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using FootballTeamManager.Modelos;
 using FootballTeamManager.Modelos.DTOs;
 using FootballTeamManager.Repositorio.IRepositorio;
 using Microsoft.AspNetCore.Http;
@@ -33,6 +34,89 @@ namespace FootballTeamManager.Controllers
             }
 
             return Ok(listaEquiposDTO);
+        }
+
+        [HttpGet("{equipoId:int}", Name = "GetEquipo")]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetEquipo( int equipoId)
+        {
+            var equipo = _repo.GetEquipo(equipoId);
+
+            if (equipo == null) return NotFound();
+
+            var equipoDTO = _mapper.Map<EquipoDTO>(equipo);
+
+            return Ok(equipoDTO);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(EquipoDTO))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CrearEquipo([FromBody] CrearEquipoDTO crearEquipoDTO)
+        {
+            if (crearEquipoDTO == null) return BadRequest(ModelState);
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (_repo.ExisteEquipo(crearEquipoDTO.Nombre))
+            {
+                ModelState.AddModelError("", "El equipo ya existe");
+                return NotFound(ModelState);
+            }
+
+            var equipo = _mapper.Map<Equipo>(crearEquipoDTO);
+
+            if (!_repo.CrearEquipo(equipo))
+            {
+                ModelState.AddModelError("", $"Algo salió mal al guardar el equipo: {equipo.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetEquipo", new { equipoId = equipo.Id }, equipo);
+        }
+
+        [HttpPatch("{equipoId:int}", Name = "PatchEquipo")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult PatchEquipo(int equipoId, [FromBody] EquipoDTO equipoDTO)
+        {
+            if (equipoDTO == null || equipoId != equipoDTO.Id) return BadRequest(ModelState);
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var equipo = _mapper.Map<Equipo>(equipoDTO);
+
+            if (!_repo.ActualizarEquipo(equipo))
+            {
+                ModelState.AddModelError("", $"Algo salió mal al actualizar el equipo: {equipo.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{equipoId:int}", Name = "EliminarEquipo")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult EliminarEquipo(int equipoId)
+        {
+            if (!_repo.ExisteEquipo(equipoId)) return NotFound();
+
+            var equipo = _repo.GetEquipo(equipoId);
+
+            if (!_repo.BorrarEquipo(equipo))
+            {
+                ModelState.AddModelError("", $"Algo salió mal al eliminar el equipo: {equipo.Nombre}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
         }
     }
 }
